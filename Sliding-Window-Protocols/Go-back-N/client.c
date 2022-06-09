fbf077058703e805219d34b3aee8041f135ff79a
aromalsanthosh <aromalsanthosh@protonmail.com>
2022-06-09 11:32:19 +0530

    Replaced with short pgm GoBackN

--- Sliding-Window-Protocols/Go-back-N/client.c
+++ Sliding-Window-Protocols/Go-back-N/client.c
@@ -1,185 +1,71 @@
-/*
-!important
-First compile server.c by gcc server.c -lm -o server
-run ./server 5400 in cmd
-Compile client.c by gcc client.c -o client
-run ./client 127.0.0.1 5400
-enter send.txt when enter file name to recieve is prompted
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+int main()
+{
+    int c_sock;
+    c_sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in client;
+    memset(&client, 0, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(9009);
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-
-
-// client code for UDP socket programming 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-
-#define IP_PROTOCOL 0 
-
-#define LEN 512 
-#define KEY 'H'
-#define TRUE 1
-#define FALSE 0
-
-#define SUCCESS 0
-#define FAILURE 1
-
-#define RRQ     1
-#define DATA    2
-#define ACK     3
-#define ERROR   4
-
-#define TYPE_INDEX          0
-#define WINDOW_SIZE_INDEX   1
-#define SEQ_NUM_INDEX       1
-#define FILE_NAME_INDEX     2
-
-#define RED     printf("\033[0;31m");
-#define GREEN   printf("\033[0;32m");
-#define DEFAULT printf("\033[0m"); 
-
-// please first look at server's code.
-
-int getType(char packet[LEN]){
-    return packet[TYPE_INDEX]-'0';
-}
-int getWindowSize(char packet[LEN]){
-    return packet[WINDOW_SIZE_INDEX]-'0';
-}
-char getSeqNum(char packet[LEN]){
-    return packet[SEQ_NUM_INDEX];
-}
-char* getFileName(char packet[LEN]){
-    char* file_name = malloc(20);
-    for(int i=0;i<20;i++){
-        file_name[i] = packet[FILE_NAME_INDEX + i];
+    if (connect(c_sock, (struct sockaddr *)&client, sizeof(client)) == -1)
+    {
+        printf("Connection failed");
+        return 0;
     }
-    file_name[19]='\0';
-    printf("File in func %s\n", file_name );
-    return file_name;
-}
-void flush_packet(char packet[LEN]){
-    int i = 0;
-    for(i=0;i<LEN;i++){
-        packet[i]='\0';
-    }
-}
-void show_data(char packet[LEN]){
-    for(int i=2;i<LEN;i++){
-        printf("%c", packet[i]);
-    }
-    return;
-}
-// to send ack to server of given seq_num
-int send_ack(int sockfd, struct sockaddr_in my_addr, char seq_num){
-    char packet[LEN];
-    flush_packet(packet);
-    // '3' = ACK
-    packet[0] = '3';//packet type
-    packet[1] = seq_num;
-    int len = sizeof(my_addr);
-    int sendBits = sendto(sockfd, packet, LEN, 0, (struct sockaddr*)&my_addr, len);
-    if(sendBits<=0){
-        RED printf("Error while writing...\n"); DEFAULT
-        return FAILURE;
-    }
-    return SUCCESS;
-}
-
-void receive_file_go_back_n(int sockfd, struct sockaddr_in my_addr){
-    int addrlen = sizeof(my_addr);
-    int req_seq_num = 0;
-    while(TRUE){
-        char packet[LEN];
-        int read_bits = recvfrom(sockfd, packet, LEN, 0, (struct sockaddr*)&my_addr, &addrlen); 
-        if(read_bits<=0){
-            printf("Error in reading...\n");
-            return;
-        }   
-        int type    =   getType(packet);
-        if(type == ERROR){
-            printf("File Not Found !!!\n");
-            return;
-        }else if(type != DATA){
-            continue;
+    printf("\n\tClient -with individual acknowledgement scheme\n\n");
+    char msg1[50] = "akwnowledgementof-";
+    char msg2[50];
+    //	char msg3[50]="Final cumulative aknowledgement";
+    char buff[100];
+    int flag = 1, flg = 1;
+    for (int i = 0; i <= 9; i++)
+    {
+        flg = 1;
+        bzero(buff, sizeof(buff));
+        bzero(msg2, sizeof(msg2));
+        if (i == 8 && flag == 1)
+        {
+            printf("here\n"); // simulating loss
+            // i--;
+            flag = 0;
+            read(c_sock, buff, sizeof(buff));
         }
-        int seq_num =   getSeqNum(packet);
-
-        if(seq_num == req_seq_num){
-            GREEN printf("Recieved Packet of seq_num : %d\n", seq_num); DEFAULT
-            printf("Sending ACK of seq_num : %d\n", req_seq_num);
-
-            if(send_ack(sockfd, my_addr, req_seq_num)==FAILURE){
-                RED printf("ACK was not sent successfully...\n"); DEFAULT
-            }else{
-                printf("ACK Sent Successfully\n");
-            }
-            req_seq_num++;
-        }else{
-            RED printf("Recieved Packet of seq_num : %d\n But Required was %d\n", seq_num, req_seq_num); DEFAULT
-            
+        int n = read(c_sock, buff, sizeof(buff));
+        if (buff[strlen(buff) - 1] != i + '0')
+        { // outoforder
+            printf("Discarded as out of order \n");
+            // printf("%c, %c, -- %s\n",buff[strlen(buff)-1],'0'+i,buff);
+            // flg=0;
+            i--;
+        }
+        else
+        {
+            printf("Message received from server : %s \n", buff);
+            printf("Aknowledgement sent for message \n");
+            strcpy(msg2, msg1);
+            msg2[strlen(msg2)] = i + '0';
+            // if(i==8)
+            // continue; 	//we will send cumulative for last 2
+            // if(i==9){
+            // write(c_sock,msg3,sizeof(msg3));
+            // printf("- %s",msg3);}
+            // else
+            write(c_sock, msg2, sizeof(msg2));
+            // printf("- %s",msg2);
         }
     }
+
+    close(c_sock);
+    return 0;
 }
-void sendRequest(int sockfd, struct sockaddr_in my_addr, char file_name[20]){
-    // type + window_size + name 
-    //   1   +      1      + 20   = 22
-    char packet[22];
-    int window_size = 1;
-    packet[0]='1';
-    packet[1]=window_size;
-    int i;
-    for(i=2;i<22;i++){
-        packet[i] = file_name[i-2];
-    }
-    int addrlen = sizeof(my_addr);
-    int sendBits = sendto(sockfd, packet, 22, 0, (struct sockaddr*)&my_addr, addrlen); 
-    if(sendBits <= 0){
-        RED printf("Error in sending....\n"); DEFAULT
-    }
-}
-
-// driver code 
-int main(int argc, char *argv[]) 
-{   
-	if(argc != 3){
-		fprintf(stderr,"Usage: %s hostname port\n", argv[0]); //Validating cmd arguments
-	}
-    // ######################### Initialization #########################
-	int PORT_NO = atoi(argv[2]);
-	char IP_ADDRESS[50];
-	strcpy(IP_ADDRESS, argv[1]);
-
-	struct sockaddr_in my_addr; 
-	int addrlen = sizeof(my_addr); 
-	my_addr.sin_family = AF_INET; 
-	my_addr.sin_port = htons(PORT_NO); 
-	my_addr.sin_addr.s_addr = inet_addr(IP_ADDRESS); 
-	// ##################################################################
-    
-    // ###################### Create Socket #############################
-	int sockfd = socket(AF_INET, SOCK_DGRAM, 
-					IP_PROTOCOL); 
-	if (sockfd < 0) 
-		printf("\nfile descriptor not received!!\n"); 
-	else
-		printf("\nfile descriptor %d received\n", sockfd); 
-    // ##################################################################
-
-
-	while (1) { 
-        char file_name[20];
-		printf("\nPlease enter file name to receive:\n"); 
-		scanf("%s", file_name); 
-        sendRequest(sockfd, my_addr, file_name);
-
-        receive_file_go_back_n(sockfd, my_addr);
-	} 
-	return 0; 
-} 
-
--- Sliding-Window-Protocols/Selective-Repeat/server.c
+++ Sliding-Window-Protocols/Selective-Repeat/server.c
@@ -8,105 +8,120 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
-
-void rsendd(int ch, int c_sock)
-{
-    char buff2[60];
-    bzero(buff2, sizeof(buff2));
-    strcpy(buff2, "reserver message :");
-    buff2[strlen(buff2)] = (ch) + '0';
-    buff2[strlen(buff2)] = '\0';
-    printf("Resending Message to client :%s \n", buff2);
-    write(c_sock, buff2, sizeof(buff2));
-    usleep(1000);
-}
-
 int main()
 {
-    int s_sock, c_sock;
-    s_sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server, other;
-    memset(&server, 0, sizeof(server));
-    memset(&other, 0, sizeof(other));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(9009);
-    server.sin_addr.s_addr = INADDR_ANY;
-    socklen_t add;
+	int s_sock, c_sock;
+	s_sock = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in server, other;
+	memset(&server, 0, sizeof(server));
+	memset(&other, 0, sizeof(other));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(9009);
+	server.sin_addr.s_addr = INADDR_ANY;
+	socklen_t add;
 
-    if (bind(s_sock, (struct sockaddr *)&server, sizeof(server)) == -1)
-    {
-        printf("Binding failed\n");
-        return 0;
-    }
+	if (bind(s_sock, (struct sockaddr *)&server, sizeof(server)) == -1)
+	{
+		printf("Binding failed\n");
+		return 0;
+	}
+	printf("\tServer Up\n Go back n (n=3) used to send 10 messages \n\n");
+	listen(s_sock, 10);
+	add = sizeof(other);
+	c_sock = accept(s_sock, (struct sockaddr *)&other, &add);
+	time_t t1, t2;
+	char msg[50] = "server message :";
+	char buff[50];
+	int flag = 0;
 
-    printf("\tServer Up\n Selective repeat scheme\n\n");
-    listen(s_sock, 10);
-    add = sizeof(other);
-    c_sock = accept(s_sock, (struct sockaddr *)&other, &add);
-    time_t t1, t2;
-    char msg[50] = "server message :";
-    char buff[50];
-    int flag = 0;
+	fd_set set1, set2, set3;
+	struct timeval timeout1, timeout2, timeout3;
+	int rv1, rv2, rv3;
 
-    fd_set set1, set2, set3;
-    struct timeval timeout1, timeout2, timeout3;
-    int rv1, rv2, rv3;
+	int i = -1;
+qq:
+	i = i + 1;
+	bzero(buff, sizeof(buff));
+	char buff2[60];
+	bzero(buff2, sizeof(buff2));
+	strcpy(buff2, "server message :");
+	buff2[strlen(buff2)] = i + '0';
+	buff2[strlen(buff2)] = '\0';
+	printf("Message sent to client :%s \n", buff2);
+	write(c_sock, buff2, sizeof(buff2));
+	usleep(1000);
+	i = i + 1;
+	bzero(buff2, sizeof(buff2));
+	strcpy(buff2, msg);
+	buff2[strlen(msg)] = i + '0';
+	printf("Message sent to client :%s \n", buff2);
+	write(c_sock, buff2, sizeof(buff2));
+	i = i + 1;
+	usleep(1000);
+qqq:
+	bzero(buff2, sizeof(buff2));
+	strcpy(buff2, msg);
+	buff2[strlen(msg)] = i + '0';
+	printf("Message sent to client :%s \n", buff2);
+	write(c_sock, buff2, sizeof(buff2));
+	FD_ZERO(&set1);
+	FD_SET(c_sock, &set1);
+	timeout1.tv_sec = 2;
+	timeout1.tv_usec = 0;
 
-    int tot = 0;
-    int ok[20];
-    memset(ok, 0, sizeof(ok));
+	rv1 = select(c_sock + 1, &set1, NULL, NULL, &timeout1);
+	if (rv1 == -1)
+		perror("select error ");
+	else if (rv1 == 0)
+	{
+		printf("Going back from %d:timeout \n", i);
+		i = i - 3;
+		goto qq;
+	}
+	else
+	{
+		read(c_sock, buff, sizeof(buff));
+		printf("Message from Client: %s\n", buff);
+		i++;
+		if (i <= 9)
+			goto qqq;
+	}
+qq2:
+	FD_ZERO(&set2);
+	FD_SET(c_sock, &set2);
+	timeout2.tv_sec = 3;
+	timeout2.tv_usec = 0;
+	rv2 = select(c_sock + 1, &set2, NULL, NULL, &timeout2);
+	if (rv2 == -1)
+		perror("select error "); // an error accured
+	else if (rv2 == 0)
+	{
+		printf("Going back from %d:timeout on last 2\n", i - 1);
+		i = i - 2;
+		bzero(buff2, sizeof(buff2));
+		strcpy(buff2, msg);
+		buff2[strlen(buff2)] = i + '0';
+		write(c_sock, buff2, sizeof(buff2));
+		usleep(1000);
+		bzero(buff2, sizeof(buff2));
+		i++;
+		strcpy(buff2, msg);
+		buff2[strlen(buff2)] = i + '0';
+		write(c_sock, buff2, sizeof(buff2));
+		goto qq2;
+	} // a timeout occured
+	else
+	{
+		read(c_sock, buff, sizeof(buff));
+		printf("Message from Client: %s\n", buff);
+		bzero(buff, sizeof(buff));
+		read(c_sock, buff, sizeof(buff));
+		printf("Message from Client: %s\n", buff);
+	}
 
-    while (tot < 9)
-    {
-        int toti = tot;
-        for (int j = (0 + toti); j < (3 + toti); j++)
-        {
-            // printf("%d %d %d \n",tot,toti,j);
-            bzero(buff, sizeof(buff));
-            char buff2[60];
-            bzero(buff2, sizeof(buff2));
-            strcpy(buff2, "server message :");
-            buff2[strlen(buff2)] = (j) + '0';
-            buff2[strlen(buff2)] = '\0';
-            printf("Message sent to client :%s \n", buff2);
-            write(c_sock, buff2, sizeof(buff2));
-            usleep(1000);
-        }
-        for (int k = 0 + toti; k < (toti + 3); k++)
-        {
-        qq:
-            FD_ZERO(&set1);
-            FD_SET(c_sock, &set1);
-            timeout1.tv_sec = 2;
-            timeout1.tv_usec = 0;
+	//}
 
-            rv1 = select(c_sock + 1, &set1, NULL, NULL, &timeout1);
-            if (rv1 == -1)
-                perror("select error ");
-            else if (rv1 == 0)
-            {
-                printf("Timeout for message :%d \n", k);
-                rsendd(k, c_sock);
-                goto qq;
-            } // a timeout occured
-            else
-            {
-                read(c_sock, buff, sizeof(buff));
-                printf("Message from Client: %s\n", buff);
-                if (buff[0] == 'n')
-                {
-                    printf(" corrupt message awk (msg %d) \n", buff[strlen(buff) - 1] - '0');
-                    rsendd((buff[strlen(buff) - 1] - '0'), c_sock);
-                    goto qq;
-                }
-                else
-                    tot++;
-                // printf("%d %d %d \n",tot,toti,k);
-            }
-        }
-    }
-
-    close(c_sock);
-    close(s_sock);
-    return 0;
+	close(c_sock);
+	close(s_sock);
+	return 0;
 }
