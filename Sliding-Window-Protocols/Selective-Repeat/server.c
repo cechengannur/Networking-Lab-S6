44d6438908460cca370bcbc29641936e6a51cb37
aromalsanthosh <aromalsanthosh@protonmail.com>
2022-06-09 11:38:12 +0530

    Change program

--- Sliding-Window-Protocols/Selective-Repeat/server.c
+++ Sliding-Window-Protocols/Selective-Repeat/server.c
@@ -8,6 +8,18 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+
+void rsendd(int ch, int c_sock)
+{
+	char buff2[60];
+	bzero(buff2, sizeof(buff2));
+	strcpy(buff2, "reserver message :");
+	buff2[strlen(buff2)] = (ch) + '0';
+	buff2[strlen(buff2)] = '\0';
+	printf("Resending Message to client :%s \n", buff2);
+	write(c_sock, buff2, sizeof(buff2));
+	usleep(1000);
+}
 int main()
 {
 	int s_sock, c_sock;
@@ -25,7 +37,7 @@ int main()
 		printf("Binding failed\n");
 		return 0;
 	}
-	printf("\tServer Up\n Go back n (n=3) used to send 10 messages \n\n");
+	printf("\tServer Up\n Selective repeat scheme\n\n");
 	listen(s_sock, 10);
 	add = sizeof(other);
 	c_sock = accept(s_sock, (struct sockaddr *)&other, &add);
@@ -38,88 +50,59 @@ int main()
 	struct timeval timeout1, timeout2, timeout3;
 	int rv1, rv2, rv3;
 
-	int i = -1;
-qq:
-	i = i + 1;
-	bzero(buff, sizeof(buff));
-	char buff2[60];
-	bzero(buff2, sizeof(buff2));
-	strcpy(buff2, "server message :");
-	buff2[strlen(buff2)] = i + '0';
-	buff2[strlen(buff2)] = '\0';
-	printf("Message sent to client :%s \n", buff2);
-	write(c_sock, buff2, sizeof(buff2));
-	usleep(1000);
-	i = i + 1;
-	bzero(buff2, sizeof(buff2));
-	strcpy(buff2, msg);
-	buff2[strlen(msg)] = i + '0';
-	printf("Message sent to client :%s \n", buff2);
-	write(c_sock, buff2, sizeof(buff2));
-	i = i + 1;
-	usleep(1000);
-qqq:
-	bzero(buff2, sizeof(buff2));
-	strcpy(buff2, msg);
-	buff2[strlen(msg)] = i + '0';
-	printf("Message sent to client :%s \n", buff2);
-	write(c_sock, buff2, sizeof(buff2));
-	FD_ZERO(&set1);
-	FD_SET(c_sock, &set1);
-	timeout1.tv_sec = 2;
-	timeout1.tv_usec = 0;
+	int tot = 0;
+	int ok[20];
+	memset(ok, 0, sizeof(ok));
 
-	rv1 = select(c_sock + 1, &set1, NULL, NULL, &timeout1);
-	if (rv1 == -1)
-		perror("select error ");
-	else if (rv1 == 0)
+	while (tot < 9)
 	{
-		printf("Going back from %d:timeout \n", i);
-		i = i - 3;
-		goto qq;
-	}
-	else
-	{
-		read(c_sock, buff, sizeof(buff));
-		printf("Message from Client: %s\n", buff);
-		i++;
-		if (i <= 9)
-			goto qqq;
-	}
-qq2:
-	FD_ZERO(&set2);
-	FD_SET(c_sock, &set2);
-	timeout2.tv_sec = 3;
-	timeout2.tv_usec = 0;
-	rv2 = select(c_sock + 1, &set2, NULL, NULL, &timeout2);
-	if (rv2 == -1)
-		perror("select error "); // an error accured
-	else if (rv2 == 0)
-	{
-		printf("Going back from %d:timeout on last 2\n", i - 1);
-		i = i - 2;
-		bzero(buff2, sizeof(buff2));
-		strcpy(buff2, msg);
-		buff2[strlen(buff2)] = i + '0';
-		write(c_sock, buff2, sizeof(buff2));
-		usleep(1000);
-		bzero(buff2, sizeof(buff2));
-		i++;
-		strcpy(buff2, msg);
-		buff2[strlen(buff2)] = i + '0';
-		write(c_sock, buff2, sizeof(buff2));
-		goto qq2;
-	} // a timeout occured
-	else
-	{
-		read(c_sock, buff, sizeof(buff));
-		printf("Message from Client: %s\n", buff);
-		bzero(buff, sizeof(buff));
-		read(c_sock, buff, sizeof(buff));
-		printf("Message from Client: %s\n", buff);
-	}
+		int toti = tot;
+		for (int j = (0 + toti); j < (3 + toti); j++)
+		{
+			// printf("%d %d %d \n",tot,toti,j);
+			bzero(buff, sizeof(buff));
+			char buff2[60];
+			bzero(buff2, sizeof(buff2));
+			strcpy(buff2, "server message :");
+			buff2[strlen(buff2)] = (j) + '0';
+			buff2[strlen(buff2)] = '\0';
+			printf("Message sent to client :%s \n", buff2);
+			write(c_sock, buff2, sizeof(buff2));
+			usleep(1000);
+		}
+		for (int k = 0 + toti; k < (toti + 3); k++)
+		{
+		qq:
+			FD_ZERO(&set1);
+			FD_SET(c_sock, &set1);
+			timeout1.tv_sec = 2;
+			timeout1.tv_usec = 0;
 
-	//}
+			rv1 = select(c_sock + 1, &set1, NULL, NULL, &timeout1);
+			if (rv1 == -1)
+				perror("select error ");
+			else if (rv1 == 0)
+			{
+				printf("Timeout for message :%d \n", k);
+				rsendd(k, c_sock);
+				goto qq;
+			} // a timeout occured
+			else
+			{
+				read(c_sock, buff, sizeof(buff));
+				printf("Message from Client: %s\n", buff);
+				if (buff[0] == 'n')
+				{
+					printf(" corrupt message awk (msg %d) \n", buff[strlen(buff) - 1] - '0');
+					rsendd((buff[strlen(buff) - 1] - '0'), c_sock);
+					goto qq;
+				}
+				else
+					tot++;
+				// printf("%d %d %d \n",tot,toti,k);
+			}
+		}
+	}
 
 	close(c_sock);
 	close(s_sock);
